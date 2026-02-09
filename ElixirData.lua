23d98c5c1d92bcb49a6e26788cdd37eb32884252
Muhammad Zidane Iqbal <46362264+zidaneiqbal@users.noreply.github.com>
2026-02-09 12:31:45 +0700

    Revert to Sort Descending

--- ElixirData.lua
+++ ElixirData.lua
@@ -309,9 +309,9 @@ local function calculateElixirs(targetPoints, availableElixirs, adjustRange)
         return {}, 0
     end
 
-    -- Sort points ascending (Optimized: more efficient comparison)
+    -- Sort points Descending (Optimized: more efficient comparison)
     table.sort(availableElixirs, function(a, b) 
-        return a.points < b.points 
+        return a.points > b.points 
     end)
 
     -- Pre-allocate used table, reduced memory allocation
@@ -751,5 +751,6 @@ task.spawn(function()
         end
     end
 end)
+
 
 
